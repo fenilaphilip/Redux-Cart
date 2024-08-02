9fb82fc3d8611cd99673771073266b336b8c6003
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-08-02 17:37:32 +0200

    Creating the store for cart

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiSliceReducer from "./ui-slice";
+import cartSliceReducer from "./cart-slice"
 
 const store = configureStore({
     reducer: {
-        ui: uiSliceReducer
+        ui: uiSliceReducer,
+        cart: cartSliceReducer
     }
 });
 
