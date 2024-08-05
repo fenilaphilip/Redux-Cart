611d5badbc551d5a0a1bb74a526df0206b3af871
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-08-05 17:24:07 +0200

    code moved to another file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Notification from './components/UI/Notification';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { sendCartData } from './store/cart-slice';
+import { sendCartData } from './store/cart-action';
 
 
 let isInitial = true;
