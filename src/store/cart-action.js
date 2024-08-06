3e468020bcb0fbd48e57cfa5d61eca96c38c3bea
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-08-06 11:59:52 +0200

    Fixing the bug, no items in cart error

--- src/store/cart-action.js
+++ src/store/cart-action.js
@@ -22,7 +22,10 @@ export const fetchCartData = () => {
 
         try {
             const cartData = await fetchData();
-            dispatch(cartActions.replaceCart(cartData));
+            dispatch(cartActions.replaceCart({
+                items: cartData.items || [],
+                totalQuantity: cartData.totalQuantity
+            }));
 
         } catch (err) {
             dispatch(uiActions.showNotification({
